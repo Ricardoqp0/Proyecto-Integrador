fd759d28e447420d4eb1e5dc519326d97ed6e0ea
Ricardoqp0 <ricardoqp78@gmail.com>
2024-08-28 07:11:29 -0600

    Pruebas con pygame

--- Inicio.py
+++ Inicio.py
@@ -1,1 +1,17 @@
-print("hola mundo")
+import pygame
+import sys
+
+pygame.init()
+
+PANTALLA = pygame.display.set_mode((1200,600))
+pygame.display.set_caption('Aventura marina')
+
+BLANCO = (255, 255, 255)
+PANTALLA.fill(BLANCO)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        pygame.display.update()
